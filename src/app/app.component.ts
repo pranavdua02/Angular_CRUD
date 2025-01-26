ef76c35f64dd2aeb36a0583ee15c28f3b05ca835
pranavdua02 <duapranav11@gmail.com>
2025-01-26 12:00:49 +0530

    formcontrols added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { EmployeeModel } from './model/Employee';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular_CRUD';
+  employeeForm: FormGroup = new FormGroup({});
+  employeeObj: EmployeeModel = new EmployeeModel();
+
+  constructor(){
+    this.createForm()
+  }
+  createForm(){
+    this.employeeForm = new FormGroup({
+      empid: new FormControl(this.employeeObj.empId),
+      name: new FormControl(this.employeeObj.name),
+      city: new FormControl(this.employeeObj.city),
+      address: new FormControl(this.employeeObj.address),
+      emailId: new FormControl(this.employeeObj.emailId),
+      pinCode: new FormControl(this.employeeObj.pinCode),
+      state: new FormControl(this.employeeObj.state),
+      contactNo: new FormControl(this.employeeObj.contactNo)
+    })
+  }
 }
---
+++ src/app/model/Employee.ts
@@ -0,0 +1,22 @@
+export class EmployeeModel {
+    empId: number;
+    name: string;
+    city: string;
+    state: string;
+    emailId: string;
+    contactNo: string;
+    address: string;
+    pinCode: string;
+
+    constructor() {
+        this.address = '';
+        this.city = '';
+        this.emailId = '';
+        this.name = '';
+        this.empId = 0;
+        this.contactNo = '';
+        this.state = '';
+        this.pinCode = ''
+    }
+}
+
